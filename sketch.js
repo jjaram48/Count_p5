a865b99e1aa00aba8ec4757f2a95f3c11f7c193f
Julian Jaramillo <jjaram48@gmail.com>
2016-09-04 20:00:17 +0200

    Organizing functions

--- sketch.js
+++ sketch.js
@@ -18,33 +18,31 @@ function setup() {
 }
 
 function mousePressed() {
-  var x1 = 10;
-  var y1 = 90;
-  var x2 = mouseX;
-  var y2 = mouseY;
-  var d = int(dist(x1, y1, x2, y2));
   i++;
 }
 
-
-function draw() {
+function circle(){
   noStroke();
   fill(0);
   ellipse(width / 2, height / 2, 250, 250);
-  
+}
+
+function count(){
   // Text formatting  
   fill(255);
   // noFill();
   // stroke(255);
   rectMode(CENTER);
-  
   // textFont(fontRegular);
   textFont(fontBold);
   // textFont(fontMedium);
-
   textSize(text_Size);
   textAlign(CENTER, CENTER);
-  
   // Display text
   text(i, (width / 2) - (text_Size / 35), (height / 2) - (text_Size / 7));
+}
+
+function draw() {
+  circle();
+  count();
 }
