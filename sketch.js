f3e63b7f400b37aee9ea5e653f64bc47225764ef
Julian Jaramillo <jjaram48@gmail.com>
2016-09-04 15:31:35 +0200

    organized code

--- sketch.js
+++ sketch.js
@@ -4,16 +4,16 @@ var text_Size = 120;
 
 function preload() {
   fontRegular = loadFont("assets/DIN/DIN.ttf");
+  fontMedium = loadFont("assets/DIN/DIN Medium.ttf");
   fontBold = loadFont("assets/DIN/DIN Bold.ttf");
 }
 
 function setup() {
   createCanvas(400, 400);
-  
-  fill(0).strokeWeight(0).textSize(10);
-  textFont(fontRegular);
+
+  // textFont(fontRegular);
   // text("Font Style Normal", 10, 30);
-  textFont(fontBold);
+  // textFont(fontBold);
   // text("Font Style Bold", 10, 70);
 }
 
@@ -30,14 +30,21 @@ function mousePressed() {
 function draw() {
   noStroke();
   fill(0);
-  ellipse(width/2, height/2, 250, 250);
+  ellipse(width / 2, height / 2, 250, 250);
+  
+  // Text formatting  
   fill(255);
+  // noFill();
+  // stroke(255);
   rectMode(CENTER);
   
-  // keyPressed("c"){
-  //   i++;
-  // }
+  // textFont(fontRegular);
+  textFont(fontBold);
+  // textFont(fontMedium);
+
   textSize(text_Size);
-  textAlign(CENTER,CENTER);
-  text(i, (width/2)-(text_Size/35), (height/2)-(text_Size/7));
+  textAlign(CENTER, CENTER);
+  
+  // Display text
+  text(i, (width / 2) - (text_Size / 35), (height / 2) - (text_Size / 7));
 }
